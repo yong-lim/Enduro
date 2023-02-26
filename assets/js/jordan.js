b646fd0179063ceb812670ce51e7b56246ea818e
Yong Lim <yong.lim@gmail.com>
2023-02-26 21:05:37 +0700

    Change some of the questions and question types

--- assets/js/jordan.js
+++ assets/js/jordan.js
@@ -12,27 +12,31 @@ function postEnduroMarch23(e) {
   e.preventDefault();
 
   console.log("in postEnduroMarch23");
-  console.log("Team:");
-  console.log(e.target['team'].value);
-  console.log("hill1:");
-  console.log(e.target['hill1'].value);
+  console.log("Team:", e.target['team'].value);
+  console.log("raceclass:", e.target['raceclass'].value);
+  console.log("phone1:", e.target['phone1'].value);
+  console.log("phone2:", e.target['phone2'].value);
+  console.log("emergency1:", e.target['emergency1'].value);
+  console.log("emergency2:", e.target['emergency2'].value);
+  console.log("e.hill1:", e.target[name='hill1'].value);
+  console.log("e.hill2:", e.target[name='hill2'].value);
+  console.log("e.enduro1:", e.target[name='enduro1'].value);
+  console.log("e.enduro2:", e.target[name='enduro2'].value);
   
   const POST_URL = 'https://script.google.com/macros/s/AKfycbw8dXqyJYlhqy7z403yd8GmIzQz2Y__yyzRSZ_47JgJJOj36rxODMxydLWxBuqPFHab/exec'
   const postRequest = {
     team       : e.target['team'].value,
+    raceclass  : e.target['raceclass'].value,
     name1      : e.target['name1'].value,
     name2      : e.target['name2'].value,
     phone1     : e.target['phone1'].value,
     phone2     : e.target['phone2'].value,
     emergency1 : e.target['emergency1'].value,
     emergency2 : e.target['emergency2'].value,
-    selected   : e.target['selected'].value,
-    koth1      : e.target['koth1'].value,
-    koth2      : e.target['koth2'].value,
-    enduro1    : e.target['enduro1'].value,
-    enduro2    : e.target['enduro2'].value,
-    hill1      : e.target['hill1'].value,
-    hill2      : e.target['hill2'].value,
+    hill1      : e.target[name='hill1'].value,
+    hill2      : e.target[name='hill2'].value,
+    enduro1    : e.target[name='enduro1'].value,
+    enduro2    : e.target[name='enduro2'].value
   };
 
   if(POST_URL) {
@@ -66,6 +70,7 @@ function changeSubject(e) {
     $('#hidden-other-subject').addClass('hidden');
   }
 }
+
 
 
 
